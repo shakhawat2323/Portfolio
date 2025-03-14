9cc9d4b54a5a696eaa6bd5e18335d3f8d9675ab5
shakhawat2323 <shakhawathossain208@gmail.com>
2025-03-14 16:35:10 +0600

    68% done

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,11 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Text from "./text";
-import RocketAnimation from "./text";
-import AnimatedCard from "./text";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <AnimatedCard />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
